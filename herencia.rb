df99e4919a7d94f61ed852324912545f940706cf
Rodrigo <rodrigo.lindenmeyer.toledo@gmail.com>
2019-06-15 10:51:16 -0400

    done with third exercise

--- herencia.rb
+++ herencia.rb
@@ -9,3 +9,19 @@ class Vehicle
     @start = true
   end
 end
+
+class Car < Vehicle
+  @@instances = 0
+  def initialize()
+    @@instances +=1
+  end
+  def self.car
+    @@instances
+  end
+  def engine_start
+    puts 'El motor se ha encendido'
+  end
+end
+
+10.times {Car.new.engine_start}
+puts "Se fabricaron #{Car.car} vehiculos"
